3957895912fb799cfc88188d7c8ef3e87d1b8bf4
cass1opeia <adrian_schmidt@outlook.com>
2021-01-19 17:52:40 +0100

    redo

--- src/Uebung6/Knoten.java
+++ src/Uebung6/Knoten.java
@@ -4,5 +4,29 @@ import java.util.List;
 
 //Knoten class given by Exercise
 public class Knoten {
+	private double wert;
+	private List<Knoten> kinder;
+
+	public Knoten(double wert, List<Knoten> kinder) {
+		this.wert = wert;
+		if(kinder == null) {
+			this.kinder = new ArrayList<Knoten>();
+		} else {
+			this.kinder = kinder;
+		}
+	}
+
+	public Knoten(double wert) {
+		this.wert = wert;
+		this.kinder = new ArrayList<Knoten>();
+	}
+
+	public double getWert() {
+		return wert;
+	}
+
+	public List<Knoten> getKinder() {
+		return kinder;
+	}
 }
 
