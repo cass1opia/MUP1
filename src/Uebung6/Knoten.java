af40fab6b715a57b293df9c45e225787a981f4b1
cass1opeia <adrian_schmidt@outlook.com>
2021-01-19 17:30:52 +0100

    comment the Knoten File

--- src/Uebung6/Knoten.java
+++ src/Uebung6/Knoten.java
@@ -2,6 +2,7 @@ package Uebung6;
 import java.util.ArrayList;
 import java.util.List;
 
+//Knoten class given by Exercise
 public class Knoten {
 	private double wert;
 	private List<Knoten> kinder;
@@ -28,3 +29,4 @@ public class Knoten {
 		return kinder;
 	}
 }
+
